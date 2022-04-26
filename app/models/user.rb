77736fcffb04d0daf8919e61792746bce9bdd7b6
Kaz <takaik@yahoo.com>
2022-04-26 10:48:16 -0400

    add bcrypt, change User password col to password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+
+  has_secure_password
+  
+  validates :email, presence: true, uniqueness: true
+
   has_many :events
   has_many :event_menus
   has_many :menus, through: :event_menus
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_24_171552) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_26_144229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_24_171552) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.string "phone"
     t.string "gender"
