533e34839215d9ff21610372cfdef757ea8fccae
Kaz <takaik@yahoo.com>
2022-05-17 15:13:27 -0400

    prepare CORS for production, prepare seed file for Heroku scheduler

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,56 +7,56 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Time.zone = 'Eastern Time (US & Canada)'
 
-Menu.create!(title: "VIO", duration: 60, price: 130, description: "Sensitive area")
-Menu.create!(title: "Arms", duration: 30, price: 100, description: "Both arms")
-Menu.create!(title: "Legs", duration: 90, price: 150, description: "Both legs")
+# Menu.create!(title: "VIO", duration: 60, price: 130, description: "Sensitive area")
+# Menu.create!(title: "Arms", duration: 30, price: 100, description: "Both arms")
+# Menu.create!(title: "Legs", duration: 90, price: 150, description: "Both legs")
 
-User.create!(
-  first_name: "Kaz",
-  last_name: "Takai",
-  password: "password",
-  email: "test@test.com",
-  phone: "347-695-5677",
-  gender: "N/A",
-  zip: 10001,
-  state: "NY",
-  city: "New York",
-  address: "545 W 30th St",
-  note: "This is a note for Kaz",
-  birthday: "1984-10-24",
-  status: "System"
-)
+# User.create!(
+#   first_name: "Kaz",
+#   last_name: "Takai",
+#   password: "password",
+#   email: "test@test.com",
+#   phone: "347-695-5677",
+#   gender: "N/A",
+#   zip: 10001,
+#   state: "NY",
+#   city: "New York",
+#   address: "545 W 30th St",
+#   note: "This is a note for Kaz",
+#   birthday: "1984-10-24",
+#   status: "System"
+# )
 
-date = Date.current
+# date = Date.current
 
-90.times do
-  start_time = Time.zone.parse("10:00:00")
-  20.times do
-    BusinessTime.create!(
-      date: date,
-      time: start_time,
-      available: true
-    )
-    start_time += 1800.seconds # 30 min
-  end
-  date += 1
-end
+# 90.times do
+#   start_time = Time.zone.parse("10:00:00")
+#   20.times do
+#     BusinessTime.create!(
+#       date: date,
+#       time: start_time,
+#       available: true
+#     )
+#     start_time += 1800.seconds # 30 min
+#   end
+#   date += 1
+# end
 
 
 # This file will be executed once a day at 12:00 AM by Heroku Scheduler
 # Comment out all seeds except BusinessTime below
 # =======================
 
-# time_slot = 1800 # 30 min
-# date = Date.current
-# start_time = Time.zone.parse("10:00:00")
-# 20.times do
-#   BusinessTime.create!(
-#     date: date,
-#     time: start_time,
-#     available: true
-#   )
-#   start_time += 1800.seconds # 30 min
-# end
+time_slot = 1800 # 30 min
+date = Date.current.since(90.days)
+start_time = Time.zone.parse("10:00:00")
+20.times do
+  BusinessTime.create!(
+    date: date,
+    time: start_time,
+    available: true
+  )
+  start_time += 1800.seconds # 30 min
+end
 
 # =======================
