d80c180acdd243247f0e0d96a7cca3dd88e551f5
Kaz <takaik@yahoo.com>
2022-08-31 19:24:42 -0400

    ass sales tax to Stripe payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,8 +2,21 @@ class PaymentsController < ApplicationController
   
   def checkout
     ids = params[:id].map{|id| id}
-    p menus = Menu.where(id: ids)
-    p line_items = menus.map{|menu| {
+    menus = Menu.where(id: ids)
+    
+    service_tax = Stripe::TaxRate.create(
+      {
+        display_name: 'Service Tax',
+        inclusive: false,
+        percentage: 4.5,
+        country: 'US',
+        state: 'NY',
+        jurisdiction: 'US - NY',
+        description: 'NY Service Tax',
+      },
+    )
+
+    line_items = menus.map{|menu| {
       price_data: {
         currency: 'usd',
         product_data: {
@@ -12,6 +25,7 @@ class PaymentsController < ApplicationController
         unit_amount: (menu.price * 100).to_i,
       },
       quantity: 1,
+      tax_rates: [service_tax],
     }}
 
     Stripe.api_key = 'sk_test_51LbjXDJC2O51yZ855ICl8ekPxg9lO11Pr4eCJ380g8btGVa40U2gl7g7TH3BnMEJSIHrQxclgdk43IZJB9n87QEA001iGI5cLz'
